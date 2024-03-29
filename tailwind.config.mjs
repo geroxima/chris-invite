fd6b1845291e40bad032a6daf11931156e55947a
geroxima <geroshima123@gmail.com>
2024-03-29 19:36:26 -0400

    first and last commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'jost': ['"Jost"'],
+			}
+		},
+		cursor: {
+			'fancy': 'url("public/favicon.ico"), pointer',
+		  },
+		screens: {
+			'sm': '640px',
+			// => @media (min-width: 640px) { ... }
+	  
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+	  
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+	  
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+	  
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		  }
+	},
+	plugins: [
+		require('tailwindcss-animated')
+	],
+}
